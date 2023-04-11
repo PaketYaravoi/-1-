716fd788d3120f3c2617a3cb2ad8fcef5c68040d
Roman <roma.klimov10.94@gmail.com>
2023-04-11 22:36:04 +1100

    создал простую программу на нохождения квадрата числа

---
+++ Exampl001_tesk1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+
+// Например:
+// 4 -> 16 
+// -3 -> 9
+// -7 -> 49
+
+
+Console.WriteLine("Введите любое число и я найду его корень!");
+string input = Console.ReadLine()!;
+
+int number = int.Parse(input);
+int square = number * number;
+
+Console.WriteLine($"квадрат числа {number} равен {square}.");
